906442c7a53efc68450bb2cfe94aae9db19aa15b
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-02-11 13:56:43 +0100

    videos till 10

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
 import routes from './routes';
 
-//dotenvFlow.config();
+dotenvFlow.config();
 
 // Create Express application
 const app: Application = express();
@@ -11,7 +11,8 @@ app.use('/api', routes);
 
 export function startServer() {
 
-    app.listen(4000, function () {
-        console.log('Server is running on port: ' + 4000);
+    const PORT: number = parseInt(process.env.PORT as string) || 4000;
+    app.listen(PORT, function () {
+        console.log('Server is running on port: ' + PORT);
     });
 }
