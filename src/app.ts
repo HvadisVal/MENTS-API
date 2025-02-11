135a90796ea12aa28df83fd22066ee67791fb046
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-02-11 14:30:58 +0100

    database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
+import { testConnection } from './repository/database';
+
 import routes from './routes';
+import { test } from 'node:test';
 
 dotenvFlow.config();
 
@@ -10,6 +13,8 @@ const app: Application = express();
 app.use('/api', routes);
 
 export function startServer() {
+
+    testConnection(); // test db connection
 
     const PORT: number = parseInt(process.env.PORT as string) || 4000;
     app.listen(PORT, function () {
--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,9 @@ const router : Router = Router();
 // get, post, put, delete (CRUD)
 
 router.get('/', (req: Request, res: Response) => {
+    // connect
     res.status(200).send('Welcome to the MENTS API');
+    // disconnect
 });
 
 export default router;
